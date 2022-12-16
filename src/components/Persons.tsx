82e5106ba180c53f65e31eda4324fa61e5242ed5
Doreapp <doreapp@github.com>
2022-12-16 19:23:36 +0100

    :hammer: Introduce the person creation dialog

--- src/components/Persons.tsx
+++ src/components/Persons.tsx
@@ -5,12 +5,18 @@
 import {
   Button,
   Collapse,
+  Dialog,
+  DialogTitle,
+  DialogContent,
   List,
   ListItemButton,
   ListItemText,
   Typography,
+  TextField,
+  DialogActions,
+  FormControl,
 } from "@mui/material";
-import React, {useState} from "react";
+import React, {FormEvent, useState} from "react";
 import {IPerson} from "../models";
 import moment from "moment";
 import CallIcon from "@mui/icons-material/Call";
@@ -135,4 +141,95 @@ export function PersonsList({persons}: {persons: IPerson[]}): JSX.Element {
     );
   }
   return <List>{items}</List>;
+}
+
+/**
+ * Text field for the edit/create dialog
+ * @param props Includes:
+ * @returns TextField instance
+ */
+function Field({
+  name,
+  ...other
+}: {
+  /** Defines the `label` of the textfield, as well as its `id` and `name` */
+  name: string;
+  [key: string]: any;
+}): JSX.Element {
+  return (
+    <TextField
+      margin="dense"
+      fullWidth
+      variant="standard"
+      label={name}
+      name={name.toLowerCase()}
+      id={name.toLowerCase()}
+      {...other}
+    />
+  );
+}
+
+/**
+ * Dialog to create/edit a person.
+ * For now, only used for creation.
+ */
+export function EditPersonDialog({
+  person,
+  open,
+  onCancel,
+  onValidate,
+}: {
+  /** The person to edit (not used for now) */
+  person?: IPerson;
+
+  /** Whether the dialog is open */
+  open: boolean;
+
+  /** Callback on edition/creation cancellation */
+  onCancel: () => void;
+
+  /** Callback onedition/creation validation (with new person as param) */
+  onValidate: (person: IPerson) => void;
+}): JSX.Element {
+  const handleValidate = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const get = (name: string): string | undefined => {
+      const value = data.get(name);
+      if (typeof value === "string" && value.trim() !== "") {
+        return value.trim();
+      }
+      return undefined;
+    };
+
+    onValidate({
+      nickname: get("nickname"),
+      firstname: get("firstname"),
+      lastname: get("lastname"),
+    });
+  };
+
+  const editing = person !== undefined;
+  return (
+    <Dialog open={open} onClose={onCancel}>
+      <DialogTitle>{editing ? "Person edition" : "New person"}</DialogTitle>
+      <DialogContent>
+        <FormControl
+          component="form"
+          id="dialog-form"
+          onSubmit={handleValidate}>
+          <Field name="Nickname" autoFocus={true} />
+          <Field name="Firstname" />
+          <Field name="Lastname" />
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel}>Cancel</Button>
+        <Button type="submit" form="dialog-form">
+          {editing ? "Save changes" : "Create"}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,12 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import {IPerson} from "../models";
-import {PersonsList} from "../components/Persons";
+import {PersonsList, EditPersonDialog} from "../components/Persons";
 import {Fab} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 export default function HomePage(): JSX.Element {
+  const [creating, setCreating] = React.useState<boolean>(false);
   const persons: IPerson[] = [
     {
       nickname: "Just nickname",
@@ -37,13 +38,25 @@ export default function HomePage(): JSX.Element {
       lastname: "Just Lastname",
     },
   ];
+  const onCreatePerson = (person: IPerson): void => {
+    console.log("On create person", person);
+    setCreating(false);
+  };
   return (
     <Container maxWidth="md">
       <PersonsList persons={persons} />
+      <EditPersonDialog
+        open={creating}
+        onCancel={() => {
+          setCreating(false);
+        }}
+        onValidate={onCreatePerson}
+      />
       <Fab
         color="primary"
         aria-label="Create a new person"
-        className="fixed bottom-6 right-4">
+        className="fixed bottom-6 right-4"
+        onClick={() => setCreating(true)}>
         <AddIcon />
       </Fab>
     </Container>
